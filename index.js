116c59f986e56317462bb64b27715334b9b290b9
Justin Klaver <Klaverts@yahoo.com>
2022-09-16 19:29:29 -0400

    added validation for questions

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ function addTeam() {
         {
         name: "name",
         message: "Please enter your team member's name.",
-
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter the team member's name!");
+                return false;
+            }
+        }
     },
     {
         name: "role",
@@ -17,10 +24,26 @@ function addTeam() {
     {
         name: "id",
         message: "Please enter the team member's ID number.",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log("Please enter the team member's ID number!");
+                return false;
+            }
+        }
     },
     {
         name: "email",
         message: "Enter the team member's email address.",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter the team member's email!");
+                return false;
+            }
+        }
     }
     ])
 }
