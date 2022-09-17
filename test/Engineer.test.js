7e22c70044ebf4cad8c0e5c1e6c0cabe4beef263
Justin Klaver <Klaverts@yahoo.com>
2022-09-17 15:38:35 -0400

    edited and copied intern tests/js to engineer

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,7 @@
 const Engineer = require("../lib/Engineer");
+
+test("should set up github account in constructor", () => {
+    const github = "github";
+    const engTest = new Engineer("Foo", 1, "test@test.com", github);
+    expect(engTest.github).toBe(github);
+  });
