b3c1fd66715d6ef862989f59f085881a07681dec
Justin Klaver <Klaverts@yahoo.com>
2022-09-16 20:40:52 -0400

    member default tests added

--- test/Member.test.js
+++ test/Member.test.js
@@ -10,4 +10,31 @@ describe("Member", () => {
     //Assert
     expect(typeof(mem)).toBe("object");
     });
+    
+    //Arrange
+    it("should set name from constructor", () => {
+        const name = "NAME";
+    //Act    
+        const memName = new Member(name);
+    //Assert
+        expect(memName.name).toBe(name);
+    });
+
+    //Arrange
+    it("should set id from constructor", () => {
+        const id = 123;
+    //Act    
+        const memId = new Member("test", id);
+    //Assert
+        expect(memId.id).toBe(id);
+    });
+    
+    //Arrange
+    it("should set email from constructor", () => {
+        const email = "test@test.com";
+    //Act    
+        const memEmail = new Member("TEST", 123, email);
+    //Assert
+        expect(memEmail.email).toBe(email);
+    });
 });
