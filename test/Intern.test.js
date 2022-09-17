8618b459df0078083682746942772ef5c1fe8538
Justin Klaver <Klaverts@yahoo.com>
2022-09-17 15:26:14 -0400

    edited and copied manager test to intern

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,7 @@
 const Intern = require("../lib/Intern");
+
+test("Should set University name in the constructor", () => {
+    const university = "SCHOOL";
+    const intTest = new Intern("TEST", 123, "email@mail.com", university);
+    expect(intTest.school).toBe(university);
+  });
